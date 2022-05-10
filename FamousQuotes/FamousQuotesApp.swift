1eb5cc16f5bd9c2ca2bcf44db9ad759ad23d3205
mrcormell <d.cormell@etoncollege.org.uk>
2022-05-10 14:08:49 +0100

    Initial Commit

---
+++ FamousQuotes/FamousQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamousQuotesApp.swift
+//  FamousQuotes
+//
+//  Created by David Cormell on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FamousQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
